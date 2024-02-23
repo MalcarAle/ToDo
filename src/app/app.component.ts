5d760bed1b632452bde33f7e6cab34afdd930df7
Alexandre Malcar Veiga <alexandre.malcar@gmail.com>
2024-02-23 14:41:02 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
+import { Todo } from '../models/todo.model';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'todo';
+  //aqui eu estou criando uma variavel publica, tipando com : any(qualquer coisa) e atribuindo um array vazio
+  public todos: Todo[] = [];
+  public title: String = 'Minhas tarefas';
+  public form: FormGroup;
+
+  //toda vez que a class Component for chamada, o metodo construtor é inicializado
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(60),
+          Validators.required,
+        ]),
+      ],
+    });
+
+    this.load();
+  }
+
+  add() {
+    //pegando o valor de title la do forms
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+
+    this.todos.push(new Todo(id, title, false));
+
+    this.save();
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+    this.save();
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    if (data !== null) {
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+  }
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,4 @@
+//exporta class define a classe como publica
+export class Todo {
+  constructor(public id: Number, public title: String, public done: Boolean) {}
+}
